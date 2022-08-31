1aef65bd29439bcfee933f266d652c76a0ada187
Chris Mems <christopher.mems@.com>
2022-08-31 09:13:56 -0500

    Add event listener

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 // consts
 const input = document.getElementById("input")
-console.log(input)
+console.dir(input)
 
 const button = document.getElementById("submit-button")
 
@@ -8,7 +8,7 @@ const listElement = document.getElementById("todo-list")
 
 // Event Listeners
 
-input.addEventListener('click', function(evt){
+button.addEventListener('click', function(evt){
   const li = document.createElement('li')
   
   li.textContent = input.value
